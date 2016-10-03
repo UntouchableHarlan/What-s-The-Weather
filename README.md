a159a810d57c2ab79671872cc901288287619074
Salomon <salomonpluviose@yahoo.com>
2016-10-03 16:29:29 -0400

    whats the weather app complete

---
+++ README.md
@@ -0,0 +1,1 @@
+# What-s-The-Weather. Mobile iOS App the lets you put in the city of your preference and generates web scraping from a weather forecast website
--- What's the Weather?/ViewController.swift
+++ What's the Weather?/ViewController.swift
@@ -9,6 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var cityTextField: UITextField!
+    
+    @IBOutlet weak var webview: UIWebView!
+    
+    @IBOutlet weak var responseLabel: UILabel!
+    
+    @IBAction func seachButton(_ sender: AnyObject) {
+        var userEntered = cityTextField.text
+        
+        if (userEntered?.contains(" "))! {
+          userEntered = userEntered?.replacingOccurrences(of: " ", with: "-")
+        }
+        
+        if let url = URL(string: "http://www.weather-forecast.com/locations/\(userEntered!)/forecasts/latest") {
+            do {
+                let contents = try String(contentsOf: url, encoding: .ascii)
+//                print(contents)
+                
+                let needContent = contents
+                
+                let parsedContent = needContent.components(separatedBy: "<p class=\"summary\">")
+                
+                let p = parsedContent[1]
+//                print(p[p.startIndex])
+                
+                let answer = NSString(string: p)
+                
+                let pContent = answer.substring(to: 378)
+                print(pContent)
+                
+                webview.loadHTMLString(pContent, baseURL: nil)
+                
+            } catch {
+                responseLabel.text = "The weather for that city couldn't be found. Try again"
+            }
+        } else {
+            print("bad url")
+            print("http://www.weather-forecast.com/locations/\(userEntered!)/forecasts/latest")
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
